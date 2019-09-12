0cad7a6da8eed7a2e411ca70c35eb89251fd0dda
Satheesh Rajendran <sathnaga@linux.vnet.ibm.com>
2019-09-12 16:49:42 +0530

    Add support for monitors
    Let's add support for monitors to framework,
    this would help us capture the snapshot of system
    details through any user provided commands running
    in a predefined frequent intervals and store in a
    file in test-reports which then can be used for
    further processing later.
    
    Usage:
    
    `monitors` file in the basepath documents how
    user can create one monitor instance and running the
    test with `--enable-monitors` will allow the framework
    to enable the monitor threads run in parallel to the
    test and collect the output and monitor threads gets
    stopped at the end of tests and additional regular expression
    will help to extract useful information in the final output file.
    
    Signed-off-by: Satheesh Rajendran <sathnaga@linux.vnet.ibm.com>

--- OpTestConfiguration.py
+++ OpTestConfiguration.py
@@ -413,6 +413,12 @@ def get_parser():
     hmcgroup.add_argument(
         "--lpar-vios", help="Lpar VIOS to boot before other LPARS", default=None)
 
+    monitorgroup = parser.add_argument_group('Monitor',
+                                              'Monitor enable commands')
+    monitorgroup.add_argument("--enable-monitors", help="If set, monitors will be enabled",
+                               action='store_true', default=False)
+    monitorgroup.add_argument("--monitor-file", help="provide the monitors file, monitors given in the file will be enabled",
+                               default="./monitors")
     return parser
 
 
